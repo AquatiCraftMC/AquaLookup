266fc5c7ce810af13d95c229a0ba665b288ed97f
Seailz <81972974+IoyoCode@users.noreply.github.com>
2022-03-16 15:56:59 +0000

    Initial commit

---
+++ src/main/java/xyz/aquaticraft/aquadiscorddata/AquaDiscordData.java
@@ -0,0 +1,42 @@
+package xyz.aquaticraft.aquadiscorddata;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import xyz.aquaticraft.aquadiscorddata.Listeners.MessageSent;
+
+import javax.security.auth.login.LoginException;
+import java.util.Collections;
+import java.util.EventListener;
+
+public final class AquaDiscordData extends JavaPlugin {
+
+    JDA jda;
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("AquaLookup is starting...");
+        try {
+            JDABuilder.createDefault("OTUzNjUzNjA4MDMxODYyODM3.YjHtIg.2mp8Ua576O6VaqOPb008sIpUjCw")
+                    .setActivity(Activity.playing("play.aquaticraft.xyz"))
+                    .addEventListeners(new MessageSent())
+                    .build();
+            Bukkit.getLogger().info("AquaLookup started!");
+        } catch (LoginException e) {
+            Bukkit.getLogger().warning("Incorrect token! Shutting down.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        jda.shutdownNow();
+    }
+}
